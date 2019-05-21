0c827c323387f7d958a9b6e8bc35ba9eabd27690
loganpluo <990740109@qq.com>
2019-05-21 20:15:05 +0800

    keep混淆

--- redpointtree/src/main/java/com/github/redpointtree/annotation/InvalidateType.kt
+++ redpointtree/src/main/java/com/github/redpointtree/annotation/InvalidateType.kt
@@ -1,8 +1,11 @@
 package com.github.redpointtree.annotation
 
+import android.support.annotation.Keep
+
 /**
  * Created by loganpluo on 2019/5/18.
  */
+@Keep
 enum class InvalidateType {
 
     Point,//每解析完字段，设置@BindRedPoint的未读数量后 都刷新下@BindRedPoint
